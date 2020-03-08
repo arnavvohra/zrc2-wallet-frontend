02fa36d076ccb57da03a1e0a785f3fbc574728ec
arnavvohra <arnav.vohra@gmail.com>
2020-03-08 06:52:25 +0530

    Changing config file

--- src/config.js
+++ src/config.js
@@ -1,11 +1,3 @@
 let config = {};
 config.apiUrl = "http://localhost:4111";
-// config.grafana={
-//     host: "http://localhost:3000",
-//     path:"/d/pfqYrSTWz/developer-dashboard?orgId=1&refresh=5s&var-dappId="
-// }
-config.grafana={
-    host: "http://grafana.biconomy.io",
-    path:"/d/7bhWE-0Zz/developer-dashboard?orgId=1&refresh=5s&var-dappId="
-}
 module.exports = config;
